114658feae23eb573efd114ad25c29c95e5965cf
Michael Lin <mikelin@amazon.com>
2015-03-24 17:20:27 -0700

    created static method

--- src/OlympiadChallenge/OlympiadChallenge.java
+++ src/OlympiadChallenge/OlympiadChallenge.java
@@ -1,11 +1,19 @@
 package OlympiadChallenge;
 
 public class OlympiadChallenge {
-    public static void main(String[] args) {
-        System.out.println("Hi world!");
-    }
-
-    public static void printHi() {
-        System.out.println("Hi world!");
+    /** 
+     * The method that will generate all your art.
+     * 
+     * @param rows Number of rows in the block scheme.
+     * @param column Number of columns in the block scheme.
+     * @param blockScheme A string that specifies the block scheme to print out. Numbers are
+     * comma delimited, newlines are colon delimited. Example: If row = 1, column = 3, 
+     * blockScheme = "1,1,1" means that the block scheme will have one row, with three columns,
+     * with each column being one block tall. See the readme in the root directory for more info.
+     *
+     * @return Prints out the ASCII art representation of this block scheme. 
+     */
+    public static void generateArt(int rows, int columns, String blockScheme) {
+        String[][] art = new String
     }
 }

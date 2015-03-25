3af335e8f754a50e1fe228eaef573bf609dd56e9
Michael Lin <mikelin@amazon.com>
2015-03-25 14:44:59 -0700

    added untested tower drawing logic

--- src/OlympiadChallenge/OlympiadChallenge.java
+++ src/OlympiadChallenge/OlympiadChallenge.java
@@ -1,6 +1,10 @@
 package OlympiadChallenge;
 
 public class OlympiadChallenge {
+    private static final String CORNER = "+";
+    private static final String HORIZ_EDGE = "-";
+    private static final String VERT_EDGE = "|";
+    private static final String INWARD_EDGE = "/";
     /** 
      * The method that will generate all your art.
      * 
@@ -14,9 +18,62 @@ public class OlympiadChallenge {
      * @return Prints out the ASCII art representation of this block scheme. 
      */
     public static void generateArt(int rows, int columns, String blockScheme) {
-        int tallestBackTower = 
+        int tallestBackTower = getTallestBackTower(blockScheme); 
         int matrixColumnSize = (columns*4)+1+(rows*2);
         int matrixRowSize = (rows*2)+(tallestBackTower*3)+1;
-        String[][] art = new String[][];
+        String[][] art = new String[matrixColumnSize][matrixRowSize];
+        String[][] finishedArt = fillInBlanks(art);
+        return finishedArt;
+    }
+
+    private int getTallestBackTower(String blockScheme) {
+        String[] backRow = blockScheme.split(":", 2);
+        int tallestTower = Integer.parseInt(backRow[0]);
+        for (String oneTower : backRow) {
+            int currentTower = Integer.parseInt(oneTower);
+            if (currentTower > tallestTower) {
+                tallestTower = currentTower;
+            }
+        }
+        return tallestTower;
+    }
+
+    private String[][] fillTowerInMatrix(int towerRow, int towerColumn, int matrixRows, 
+                                         int matrixColumns, int towerHeight, String[][] matrixSoFar) {
+        int rightOffset = matrixSoFar.length - (matrixRows * 2);
+        for (int towerIndex = 0; towerIndex < towerHeight; towerIndex++) {
+            matrixSoFar[rightOffset][towerIndex * 3] = CORNER;
+            matrixSoFar[rightOffset - 1][towerIndex * 3] = HORIZ_EDGE;
+            matrixSoFar[rightOffset - 2][towerIndex * 3] = HORIZ_EDGE;
+            matrixSoFar[rightOffset - 3][towerIndex * 3] = HORIZ_EDGE;
+            matrixSoFar[rightOffset - 4][towerIndex * 3] = CORNER;
+            
+            matrixSoFar[rightOffset][(towerIndex * 3) + 1] = VERT_EDGE;
+            matrixSoFar[rightOffset][(towerIndex * 3) + 2] = VERT_EDGE;
+            
+            matrixSoFar[rightOffset - 4][(towerIndex * 3) + 1] = VERT_EDGE;
+            matrixSoFar[rightOffset - 4][(towerInex * 3) + 2] = VERT_EDGE;
+
+            if (towerToRight()) {
+                matrixSoFar[rightOffset + 1][(towerIndex * 3) + 1] = INWARD_EDGE;
+                matrixSoFar[rightOffset + 2][(towerIndex * 3) + 2] = CORNER;
+                matrixSoFar[rightOffset + 2][(towerIndex * 3) + 3] = VERT_EDGE;
+                matrixSoFar[rightOffset + 2][(towerIndex * 3) + 4] = VERT_EDGE;
+                matrixSoFar[rightOffset + 2][(towerIndex * 3) + 5] = CORNER;
+                matrixSoFar[rightOffset + 1][(towerIndex * 3) + 4] = INWARD_EDGE;
+            }
+        }
+        matrixSoFar[rightOffset][[(towerHeight - 1) * 3] + 4] = CORNER;
+        matrixSoFar[rightOffset-1][[(towerHeight - 1) * 3] + 4] = HORIZ_EDGE;
+        matrixSoFar[rightOffset-2][[(towerHeight - 1) * 3] + 4] = HORIZ_EDGE;
+        matrixSoFar[rightOffset-3][[(towerHeight - 1) * 3] + 4] = HORIZ_EDGE;
+        matrixSoFar[rightOffset-4][[(towerHeight - 1) * 3] + 4] = CORNER;
+
+        matrixSoFar[rightOffset-3][[(towerHeight - 1) * 3] + 5] = INWARD_EGE;
+        matrixSoFar[rightOffset-2][[(towerHeight - 1) * 3] + 6] = CORNER;
+        matrixSoFar[rightOffset-1][[(towerHeight - 1) * 3] + 6] = HORIZ_EDGE;
+        matrixSoFar[rightOffset][[(towerHeight - 1) * 3] + 6] = HORIZ_EDGE;
+        matrixSoFar[rightOffset+1][[(towerHeight - 1) * 3] + 6] = HORIZ_EDGE;
+        return matrixSoFar;
     }
 }

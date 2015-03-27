af687ee5a0f2f13e78f612c88941c1ebd7b6078b
Michael Lin <mikelin@amazon.com>
2015-03-26 17:11:23 -0700

    Added some comments, cleaned up code

--- src/OlympiadChallenge/OlympiadChallenge.java
+++ src/OlympiadChallenge/OlympiadChallenge.java
@@ -16,30 +16,27 @@ public class OlympiadChallenge {
      * blockScheme = "1,1,1" means that the block scheme will have one row, with three columns,
      * with each column being one block tall. See the readme in the root directory for more info.
      *
+     * Note that we are going to use row major notation for everything in the matrix representation 
+     * of the art. E.G. if we have a 2x2 matrix, (0, 0) represents the top left corner, (1, 0) represents
+     * the bottom left corner, (0, 1) represents the top right, (1, 1) is the bottom right.
+     *
      * @return Prints out the ASCII art representation of this block scheme. 
      */
     public static void generateArt(int rows, int columns, String blockScheme) {
         int tallestBackTower = getTallestBackTower(blockScheme); 
         int matrixColumnSize = (columns*4)+1+(rows*2);
         int matrixRowSize = (rows*2)+(tallestBackTower*3)+1;
-        System.out.println("tallestBackTower is: " + tallestBackTower);
-        System.out.println("matrixColumnSize is: " + matrixColumnSize);
-        System.out.println("matrixRowSize is: " + matrixRowSize);
         String[][] artInProgress = new String[matrixRowSize][matrixColumnSize];
         for (int columnIndex = 0; columnIndex < columns; columnIndex++) {
             for (int rowIndex = 0; rowIndex < rows; rowIndex++) {
                 int towerHeight = getTowerHeight(rowIndex, columnIndex, rows, columns, 
                                                  blockScheme);
-                System.out.println("towerHeight is: " + towerHeight);
                 artInProgress = fillTowerInMatrix(rowIndex, columnIndex, rows, columns,
                                                   towerHeight, artInProgress, blockScheme);
-                System.out.println("art so far is");
-                printArt(artInProgress);
             }
         }                      
         String[][] finishedArt = fillInBlanks(artInProgress);
-        //printArt(finishedArt);
-        printArt(artInProgress);
+        printArt(finishedArt);
     }
 
     private static int getTallestBackTower(String blockScheme) {
@@ -58,27 +55,18 @@ public class OlympiadChallenge {
     private static int getTowerHeight(int towerRow, int towerColumn, int numOfRows, int numOfColumns,
                                       String blockScheme) {
         String[] rows = blockScheme.split(":", numOfRows);
-        System.out.println("numOfRows is: " + numOfRows);
-        System.out.println("towerRow: " + towerRow);
-        System.out.println("rows is: " + rows);
-        System.out.println("rows[0]: " + rows[0]);
-        System.out.println("wtf: " + towerRow);
         String row = rows[towerRow];
         String[] heights = row.split(",", numOfColumns);
         return Integer.parseInt(heights[towerColumn]);
     }
 
-    private static String[][] fillTowerInMatrix(int towerRow, int towerColumn, int matrixRows, int matrixColumns,
-                                         int towerHeight, String[][] matrixSoFar, String blockScheme) {
+    private static String[][] fillTowerInMatrix(int towerRow, int towerColumn, int matrixRows,
+                                                int matrixColumns, int towerHeight, String[][] matrixSoFar,
+                                                String blockScheme) {
         int leftOffset = (towerColumn * 4) + ((matrixRows - towerRow - 1)*2);
         int vertOffset = (matrixSoFar.length - ((matrixRows - towerRow - 1) * 2));
-        System.out.println("left offset is: " + leftOffset + " " + matrixSoFar.length + " " + matrixRows);
-        System.out.println("vert offset is: " + vertOffset);
         for (int towerIndex = 0; towerIndex < towerHeight; towerIndex++) {
-            System.out.println("in for loop");
             //Fill in bottom edge
-            System.out.println("matrixRows: " + matrixRows);
-            System.out.println("towerIndex: " + towerIndex);
             matrixSoFar[vertOffset - (towerIndex * 3) - 1][leftOffset] = CORNER;
             matrixSoFar[vertOffset - (towerIndex * 3) - 1][leftOffset + 1] = HORIZ_EDGE;
             matrixSoFar[vertOffset - (towerIndex * 3) - 1][leftOffset + 2] = HORIZ_EDGE;
@@ -142,31 +130,13 @@ public class OlympiadChallenge {
         return matrix;
     }
 
-    public static void printArt(String[][] art) {
-        System.out.println("art is: " + art);
+    private static void printArt(String[][] art) {
         for (int columnIndex = 0; columnIndex < art.length; columnIndex++) {
             String row = "";
             for (int rowIndex = 0; rowIndex < art[0].length; rowIndex++) {
                 row = row + art[columnIndex][rowIndex];
             }
             System.out.println(row);
-        }
-    }
-
-    public static boolean blockToRightExists(int row, int column, int height, int numOfRows,
-                                             int numOfColumns, String blockScheme) {
-        String[] splitRows = blockScheme.split(":", numOfRows);
-        String towerRow = splitRows[row];
-        String[] splitColumns = towerRow.split(",", numOfColumns);
-        System.out.println("split Columns size: " + splitColumns.length);
-        System.out.println("column+1: " + column);
-        System.out.println("column: " + column);
-        int neighborHeight = (column+2) > numOfColumns ? -1 : Integer.parseInt(splitColumns[column]);
-        System.out.println("neighborHeight" + neighborHeight);
-        if (neighborHeight >= height) {
-            return true;
-        } else {
-            return false;
         }
     }
 }
--- tst/OlympiadChallengeTests.java
+++ tst/OlympiadChallengeTests.java
@@ -11,30 +11,39 @@ import org.junit.Test;
 public class OlympiadChallengeTests {
 
     private final OlympiadChallenge olympiadChallenge = new OlympiadChallenge();
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
-    @Before
-    public void setUp() {
-        // System.setOut(new PrintStream(outContent));
-        // System.setErr(new PrintStream(errContent));
-    }
-
-    @After
-    public void cleanUp() {
-        //    System.setOut(null);
-        //System.setErr(null);
+    @Test
+    public void testFigure1() {
+        System.out.println("Running Figure 1 Test");
+        System.out.println("Program printed this picture:");
+        olympiadChallenge.generateArt(1, 1, "1");
     }
 
     @Test
-    public void testOneBlock() {
-        System.out.println("Running OneBlock test");
-        System.out.println("Expecting this picture:");
-        System.out.println("..+---+\n./   /|\n+---+ |\n|   | +\n|   |/.\n+---+..");
+    public void testFigure2() {
+        System.out.println("Running Figure 2 Test");
+        System.out.println("Program printed this picture:");
+        olympiadChallenge.generateArt(1, 2, "1,1");
+    }
+
+    @Test
+    public void testFigure3() {
+        System.out.println("Running Figure 3 Test");
+        System.out.println("Program printed this picture:");
+        olympiadChallenge.generateArt(1, 1, "2");
+    }
+
+    @Test
+    public void testFigure4() {
+        System.out.println("Running Figure 4 Test");
+        System.out.println("Program printed this picture:");
+        olympiadChallenge.generateArt(2, 1, "1:1");
+    }
+
+    @Test
+    public void testFigure5() {
+        System.out.println("Running Figure 5 Test");
         System.out.println("Program printed this picture:");
         olympiadChallenge.generateArt(3, 4, "2,2,1,2:2,2,1,1:3,2,1,2");
-        //olympiadChallenge.generateArt(2, 2, "2,2:2,2");
-        System.err.println("test err");
-        //Assert.assertEquals("Hi world!\n", outContent.toString());
     }
 }

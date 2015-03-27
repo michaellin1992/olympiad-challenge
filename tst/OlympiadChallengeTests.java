5f33c43b260ae4184200beef93c6decb70cd8c7d
Michael Lin <mikelin@amazon.com>
2015-03-27 15:47:03 -0700

    added stairs and sinewave pictures

--- tst/OlympiadChallengeTests.java
+++ tst/OlympiadChallengeTests.java
@@ -54,11 +54,24 @@ public class OlympiadChallengeTests {
         olympiadChallenge.generateArt(5, 5, "1,1,1,1,1:1,2,2,2,1:1,2,3,2,1:1,2,2,2,1:1,1,1,1,1");
     }
     
-    
     @Test
     public void testHoles() {
         System.out.println("Running Holes Test");
         System.out.println("Program printed this picture:");
         olympiadChallenge.generateArt(5, 5, "1,1,1,1,0:1,2,2,2,1:1,2,3,2,0:1,2,2,2,1:1,1,0,1,1");
+    }
+
+    @Test
+    public void testStairs() {
+        System.out.println("Running Stairs Test");
+        System.out.println("Program printed this picture:");
+        olympiadChallenge.generateArt(4, 5, "4,4,4,4,4:3,3,3,3,3:2,2,2,2,2:1,1,1,1,1");
+    }
+
+    @Test
+    public void testSineWave() {
+        System.out.println("Running Sine Wave Test");
+        System.out.println("Program printed this picture:");
+        olympiadChallenge.generateArt(1, 17, "3,2,1,2,3,4,3,2,1,2,3,4,3,2,1,2,3");
     }
 }
